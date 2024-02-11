cf9f5a268b64c41a292ed174d813d55d8008584b
Francisco Palacios Chaves <fpalacioschaves@gmail.com>
2024-02-11 13:44:42 +0100

    add category

--- add_categoria.php
+++ add_categoria.php
@@ -4,50 +4,94 @@ include("./funciones.php");
 check_session();
 $categorias = leer_categorias();
 ?>
-<!DOCTYPE html>
-<html lang="en">
-<?php include("./head.php"); ?>
+<?php
+if ($_POST) {
+    add_categoria($_POST);
+} else {
+?>
 
-<body>
-
+    <!DOCTYPE html>
+    <?php include("./head.php"); ?>
     <?php include("./header.php"); ?>
 
-    <section class="container total_container">
-        <?php
-        if ($_POST) {
+    <body class="sb-nav-fixed">
+        <?php include("./header.php"); ?>
+        <div id="layoutSidenav">
+            <div id="layoutSidenav_nav">
+                <nav class="sb-sidenav accordion sb-sidenav-dark" id="sidenavAccordion">
+                    <div class="sb-sidenav-menu">
+                        <div class="nav">
+                            <a class="nav-link" href="add_imagen.php">
+                                Añadir imagen
+                            </a>
 
-            add_categoria($_POST);
-        } else {
-        ?>
+                            <a class="nav-link" href="add_categoria.php">
+                                Añadir categoria
+                            </a>
 
-            <form id="add_categoria" action="" method="POST">
-          
-        <div class="row fila">
-            <div class="col">
-                <label for="categoria_imagen">Nueva categoría</label>
-                <input class="form-control" type="text" name="categoria_nombre" required>
-
+                            <a class="nav-link" href="logout.php">
+                                Logout
+                            </a>
+                        </div>
+                    </div>
             </div>
+            <div class="sb-sidenav-footer">
 
-            <div class="col">
-              <input type="submit" style="margin-top: 24px;" value="Añadir" class="btn btn-primary boton_enviar">
-          </div>
-          
+                </nav>
+            </div>
+            <div id="layoutSidenav_content">
+                <main>
+                    <div class="container-fluid px-4">
+                        <h1 class="mt-4">Añadir categoría</h1>
+                        <section class="container total_container">
+                            <div class="album py-5 bg-light" style="background-color: white !important;">
+                                <div class="container">
+                                    <form id="add_categoria" action="" method="POST">
+                                        <div class="row fila">
+                                            <div class="col">
+                                                <label for="categoria_imagen">Nueva categoría</label>
+                                                <input class="form-control" type="text" name="categoria_nombre" required>
+
+                                            </div>
+
+                                            <div class="col">
+                                                <input type="submit" style="margin-top: 24px;" value="Añadir" class="btn btn-primary boton_enviar">
+                                            </div>
+
+                                        </div>
+
+                                        <div class="row fila">
+
+
+                                        </div>
+                                    </form>
+
+                                <?php } ?>
+                                </div>
+                            </div>
+                        </section>
+                    </div>
+                </main>
+                <footer class="py-4 bg-light mt-auto">
+                    <div class="container-fluid px-4">
+                        <div class="d-flex align-items-center justify-content-between small">
+                            <div class="text-muted">Copyright &copy; CENEC Grupo ATU 2024</div>
+                        </div>
+                    </div>
+                </footer>
+            </div>
         </div>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+        <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+        <script src="./js/lightbox-plus-jquery.js"></script>
+        <script src="./js/masonEasy.min.js"></script>
+        <script src="./js/scripts.js" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
+        <script src="js/scripts.js"></script>
+    </body>
 
-        <div class="row fila">
-          
-          
-      </div>
-     </form>
-
-        <?php } ?>
+    </html>
 
 
-    </section>
 
-
-    <script src="./js/scripts.js" crossorigin="anonymous"></script>
-</body>
-
-</html>
+    <!-- -->
